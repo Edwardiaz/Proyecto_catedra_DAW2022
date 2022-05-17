4d2953d189b6a3122d3b69671e55e3ce896e62fb
Ematias92 <99231316+Ematias92@users.noreply.github.com>
2022-05-16 22:04:19 -0600

    Login al sistema corregido.
    Acá ya realice los cambios para acceder al sistema.

--- js/Login.js
+++ js/Login.js
@@ -1,16 +1,10 @@
 //Control de Login
 
-var usuario = document.forms['form']['user'];
-var contraseña = document.forms ['form']['password'];
-
-var usuario_error = document.getElementById('usuario_error');
-var constraseña_error = document.getElementById('contraseña_error');
-
-function validated(){
-    if (usuario.value = 'Ash Ketchum');
-    return false;
-    {
-    if (contraseña.value = '1234');
-    return false;
+function go(){
+    if (document.form.password.value=='1234' && document.form.login.value=='Ash Ketchum'){
+        document.form.submit();
+    }
+    else{
+        alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
     }
 }
