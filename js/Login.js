abf9c4c83d66e873d3fc4a5b65f9ed9dbc38a299
Ematias92 <99231316+Ematias92@users.noreply.github.com>
2022-05-20 21:13:36 -0600

    Update Login.js
    Se agrega 1 usuario extra

--- js/Login.js
+++ js/Login.js
@@ -1,8 +1,13 @@
 //Control de Login
 
 function go(){
-    if (document.form.password.value=='1234' && document.form.login.value=='Ash Ketchum'){
+    if (document.form.password.value=='1234' && document.form.login.value=='Ash Ketchum'||document.form.password.value=='4321' && document.form.login.value=='Eduardo Matias'){
         document.form.submit();
+        Swal.fire({
+            icon: 'success',
+            title: 'Bienvenido',
+            text: 'acceso'
+        })
     }
     else{
         Swal.fire({
