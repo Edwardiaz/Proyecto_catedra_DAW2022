0d098fd01e5f8490e73c1be688f0efced6c15753
Jorge Díaz <jorgediaz9515@gmail.com>
2022-05-23 00:55:42 -0600

    commit 14
    -He terminado los metodos de Deposito y Retiro añadiendo un par de validaciones cuando ya no se tiene saldo
    
    -Cree un metodo general para todos los modulos de pago
    
    -Modifique el pdf para cada tipo de factura, añadi nuevas variables para solo usarlas segun convenga
    
    -añadi los botones para efectuar la transaccion y descargar pdf
    
    -Cree dos nuevos arrays para guardar en el LocalStorage las descripciones de las transacciones, fechas y montos correspondientes
    
    -----FALTA--------
    Modificar algunos modulos porque hay campos innecesarios
    
    terminar de modificar los ultimos tres pdf para añadirles sus variables correspondientes
    
    -FALTA EL HISTORIAL

--- js/jsPDF.js
+++ js/jsPDF.js
@@ -1,59 +1,77 @@
+function fechaActual() {
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
+    const date = new Date();
+    let dia = date.getDate();
+    let mes = meses[date.getMonth()];
+    let anio = date.getFullYear();
+    return  mes + '_' + anio;
+}
 
 function descargar( opcion ){
     var doc = new jsPDF();
-    
-    //var cuenta = document.getElementsByTagName('cuenta').value;
-    //var monto = document.getElementsByTagName('monto').value;
-
-
     //TODO: AHora falta editar cada caso para que agarre los valores del LocalStorage
-    cuenta = window.localStorage.getItem('cuenta');
+    var facturaFinal = '';
+    var cuenta = window.localStorage.getItem('cuenta');
+    var monto = window.localStorage.getItem('monto');
+    var total = window.localStorage.getItem('total');
+    var cuentaAnda = window.localStorage.getItem('cuentaAnda');
+    proveedorTel = window.localStorage.getItem('proveedorTel');
+    proveedorInter = window.localStorage.getItem('proveedorInter');
 
     switch(opcion){
         case 1:
-            doc.text("-------------------------------------------------", 20, 20);
-            doc.text("Comprobante de Deposito.", 20, 30);
-            doc.text("-------------------------------------------------", 20, 40);
-            doc.text("Numero de cuenta del monto acreditado: "+window.localStorage.getItem('monto'), 20 ,50);
-            doc.text("Monto de la transacción: "+150, 20 ,60);
+            doc.text("*******************************************************", 20, 20);
+            doc.text("********** Comprobante de Deposito **************", 20, 30);
+            doc.text("*******************************************************", 20, 40);
+            doc.text("Numero de cuenta del monto acreditado: "+cuenta, 20 ,50);
+            doc.text("Monto acreditado: $"+monto, 20 ,60);
+            doc.text("Nuevo saldo disponible: $"+total, 20 ,70);
+            facturaFinal = 'Comprobante_deposito_'+fechaActual()+'.pdf';
+            
             break;
         case 2:
-            doc.text("-------------------------------------------------", 20, 20);
-            doc.text("Comprobante de Retiro.", 20, 30);
-            doc.text("-------------------------------------------------", 20, 40);
-            doc.text("Numero de cuenta del monto retirado: -"+window.localStorage.getItem('monto'), 20 ,50);
-            doc.text("Saldo restante: "+window.localStorage.getItem('total'), 20 ,50);
-            doc.text("Monto de la transacción: "+150, 20 ,60);
+            doc.text("*******************************************************", 20, 20);
+            doc.text("************* Comprobante de Retiro **************", 20, 30);
+            doc.text("*******************************************************", 20, 40);
+            doc.text("Numero de cuenta origen del monto retirado: "+cuenta, 20, 50);
+            doc.text("Monto retirado: $"+monto, 20 ,60);
+            doc.text("Saldo restante: $"+total, 20 ,70);
+            facturaFinal = 'Comprobante_retiro_'+fechaActual()+'.pdf';
             break;
         case 3:
-            doc.text("-------------------------------------------------", 20, 20);
-            doc.text("Comprobante pago de Servicios de agua potable.", 20, 30);
-            doc.text("-------------------------------------------------", 20, 40);
-            doc.text("Numero de cuenta del monto acreditado: "+250, 20 ,50);
-            doc.text("Monto de la transacción: "+150, 20 ,60);
+            doc.text("*****************************************************************************", 20, 20);
+            doc.text("*********** Comprobante pago de Servicios de agua potable *********", 20, 30);
+            doc.text("*****************************************************************************", 20, 40);
+            doc.text("Numero de cuenta origen del monto debitado: "+cuenta, 20 ,50);
+            doc.text("numero cuenta de usuario Anda: "+cuentaAnda, 20 ,70);
+            doc.text("Monto debitado: $"+monto, 20 ,60);
+            facturaFinal = 'Comprobante_pago_ANDA_'+fechaActual()+'.pdf';
             break;
         case 4:
-            doc.text("-------------------------------------------------", 20, 20);
-            doc.text("Comprobante pago de Servicios de energía eléctrica.", 20, 30);
-            doc.text("-------------------------------------------------", 20, 40);
+            doc.text("**********************************************************************", 20, 20);
+            doc.text("********* Comprobante pago de Servicios de energía eléctrica *********", 20, 30);
+            doc.text("**********************************************************************", 20, 40);
             doc.text("Numero de cuenta del monto acreditado: "+250, 20 ,50);
             doc.text("Monto de la transacción: "+150, 20 ,60);
+            facturaFinal = 'Comprobante_pago_CAESS_'+fechaActual()+'.pdf';
             break;
         case 5:
-            doc.text("-------------------------------------------------", 20, 20);
-            doc.text("Comprobante pago de Servicio de Internet.", 20, 30);
-            doc.text("-------------------------------------------------", 20, 40);
+            doc.text("***************************************************************", 20, 20);
+            doc.text(" ********* Comprobante pago de Servicio de Internet *********", 20, 30);
+            doc.text("***************************************************************", 20, 40);
             doc.text("Numero de cuenta del monto acreditado: "+250, 20 ,50);
             doc.text("Monto de la transacción: "+150, 20 ,60);
+            facturaFinal = 'Comprobante_pago_Internet_'+fechaActual()+'.pdf';
             break;
         default:
-            doc.text("-------------------------------------------------", 20, 20);
-            doc.text("Comprobante pago de Servicio de telefonía.", 20, 30);
-            doc.text("-------------------------------------------------", 20, 40);
+            doc.text("***************************************************************", 20, 20);
+            doc.text(" ********* Comprobante pago de Servicio de telefonía *********", 20, 30);
+            doc.text("***************************************************************", 20, 40);
             doc.text("Numero de cuenta del monto acreditado: "+250, 20 ,50);
             doc.text("Monto de la transacción: "+150, 20 ,60);
+            facturaFinal = 'Comprobante_pago_telefonia_'+fechaActual()+'.pdf';
     }
     doc.setFont("courier", "normal");
-    doc.save("Prueba.pdf");
+    doc.save(facturaFinal);
 }
--- js/utils.js
+++ js/utils.js
@@ -5,28 +5,25 @@ function calcularFechaActual() {
     let dia = date.getDate();
     let mes = meses[date.getMonth()];
     let anio = date.getFullYear();
-    //document.getElementById("fecha").innerHTML = dia + ' ' + mes + ' ' + anio;
-    return dia + ' ' + mes + ' ' + anio;
+    return dia + ' de ' + mes + ' de ' + anio;
 }
 
 var total = 0;
-
-//metodo funcional con localStorage para Desposito, falta añadir esto para el PDF
-//FALTA AÑADIR UNA DESCRIPCION DE LA TRANSACCION... posiblemente se haga en otro array...
-
-// Ya descarga el pdf en Retiro y Deposito, falta añadirlo a los demas modulos, el historial 
 // se puede sacar directo en historial con un OnLoad()...
 
 function agregar() {
 
     var montoString = document.getElementById("monto").value;
+    var cuenta = document.getElementById('cuentaOrigen').value;
     var monto = parseFloat(montoString);
     var montoArray = [];
     var descripcionArray = []; 
+    var fechaArray = [];
     //deacuerdo al orden que se guarde cada monto se guardara una descripcion que solo existira 
     //internamente, en el caso del modulo de Deposito sera 'Deposito'
     //monto tiene que ser una variable normal, hay que crear un array donde se guarde monto para usarla en historial y monto para sumarla con total
     window.localStorage.setItem('monto', monto);
+    window.localStorage.setItem('cuenta', cuenta);
 
     total = window.localStorage.getItem('total');
 
@@ -38,14 +35,30 @@ function agregar() {
     }
     window.localStorage.setItem('total', total);
 
+    //aqui consigo el array del localStorage, pero si es la primera vez que lo consulta entonces guarda en el indice 0 un valor vacio, por
+    //lo que se hace una evaluacion y se inserta el monto en caso venga vacio con un lngth igual a 1
     montoArray = JSON.parse("[" + window.localStorage.getItem('montoArray') + "]");
+    descripcionArray = [ window.localStorage.getItem('descripcionArray') ];
+    fechaArray = [window.localStorage.getItem('fechaArray')];
     if(montoArray[0] == null && montoArray.length == 1){
         montoArray[0] = monto;
+        descripcionArray[0] = 'Deposito';
+        fechaArray[0] = calcularFechaActual();
         window.localStorage.setItem('montoArray', montoArray);
+        window.localStorage.setItem('descripcionArray', descripcionArray);
+        window.localStorage.setItem('fechaArray', fechaArray);
+
     } else {
         montoArray.push(monto);
+        descripcionArray.push('Deposito');
+        fechaArray.push(calcularFechaActual());
         window.localStorage.setItem('montoArray', montoArray);
+        window.localStorage.setItem('descripcionArray', descripcionArray);
+        window.localStorage.setItem('fechaArray', fechaArray);
         console.log(montoArray);
+        console.log(descripcionArray);
+        console.log(fechaArray);
+        
     }
 /*
     celda = document.createElement("td");
@@ -59,45 +72,112 @@ function agregar() {
     //DA problema debido a que el trDatos no esta en la pagina donde toy creando el nodo nuevo, F
     window.localStorage.setItem('trTag', trTag.appendChild(celda));
     */
-   mostrarDatos(monto, total, );
 }
 
-function mostrarDatos(){
+function pagoFactura(factura) {
+    var montoString = document.getElementById("monto").value;
+    var monto = parseFloat(montoString);
+    var cuenta = document.getElementById('cuentaOrigen').value;
+    var codigoFactura = '';
+    var montoArray = [];
+    var descripcionArray = [];
+    var fechaArray = [];
+
+    window.localStorage.setItem('monto', monto);
+    window.localStorage.setItem('cuenta', cuenta);
+    
+
+    total = window.localStorage.getItem('total');
+
+    if(total != null && total >= monto) {
+        
+        total = parseFloat(total) - monto;
+    
+        window.localStorage.setItem('total', total);
+        
+        montoArray = JSON.parse("[" + window.localStorage.getItem('montoArray') + "]");
+        descripcionArray = [ window.localStorage.getItem('descripcionArray') ];
+        fechaArray = [window.localStorage.getItem('fechaArray')];
+
+        if(montoArray[0] == null && montoArray.length == 1){
+                alert('No tiene suficientes fondos para efectuar el pago'); // AQUI TIENES QUE APLICAR LO DE LA LIBRERIA DE SWEETALERT Eduardo
+        } else {
+            montoArray.push(-1*monto);
+            
+            switch(factura){
+                case 'anda':
+                    descripcionArray.push('Pago ANDA');
+                    codigoFactura = document.getElementById('num').value;
+                    window.localStorage.setItem('cuentaAnda', codigoFactura);
+                break;
+                case 'caess':
+                    descripcionArray.push('Pago CAESS');
+                    codigoFactura = document.getElementById('nic').value;
+                    window.localStorage.setItem('cuentaAnda', codigoFactura);
+                break;
+                case 'Internet':
+                    descripcionArray.push('Pago Internet');
+                    proveedorInter = document.getElementById('proveedor').value;
+                    window.localStorage.setItem('proveedorInter', proveedorInter);
+                break;
+                case 'telefono':
+                    proveedorTel = document.getElementById('proveedor').value;
+                    window.localStorage.setItem('proveedorTel', proveedorTel);
+                    descripcionArray.push('Pago telefonía');
+                break;
+
+            }
+            window.localStorage.setItem('montoArray', montoArray);
+            window.localStorage.setItem('descripcionArray', descripcionArray);
+            window.localStorage.setItem('fechaArray', fechaArray);
+            console.log(montoArray);
+            console.log(descripcionArray);
+            console.log(fechaArray);
+        }
+    } else {
+        alert('No tiene suficientes fondos para efectuar el pago'); // AQUI TIENES QUE APLICAR LO DE LA LIBRERIA DE SWEETALERT Eduardo
+    }
 
 }
 
-//Falta probar este metodo en la seccion de retiros
 function retiro() {
 
     var montoString = document.getElementById("monto").value;
     var monto = parseFloat(montoString);
+    var cuenta = document.getElementById('cuentaOrigen').value;
     var montoArray = [];
-    //monto tiene que ser una variable normal, hay que crear un array donde se guarde monto para usarla en historial y monto para sumarla con total
+    var descripcionArray = []; 
+    var fechaArray = [];
+    
     window.localStorage.setItem('monto', monto);
+    window.localStorage.setItem('cuenta', cuenta);
 
     total = window.localStorage.getItem('total');
 
-    if(total == null || total == 0) {
-        total = monto;
-    } else {
-
+    if(total != null && total >= monto) {
+        
         total = parseFloat(total) - monto;
-    }
     
-    window.localStorage.setItem('total', total);
-    
-    montoArray = JSON.parse("[" + window.localStorage.getItem('montoArray') + "]");
+        window.localStorage.setItem('total', total);
+        
+        montoArray = JSON.parse("[" + window.localStorage.getItem('montoArray') + "]");
+        descripcionArray = [ window.localStorage.getItem('descripcionArray') ];
+        fechaArray = [window.localStorage.getItem('fechaArray')];
 
-    if(montoArray[0] == null && montoArray.length == 1){
-        /*
-        montoArray[0] = monto;
-        window.localStorage.setItem('montoArray', montoArray);
-        */
-        alert('No tiene suficientes fondos para harec un retiro'); // AQUI TIENES QUE APLICAR LO DE LA LIBRERIA DE SWEETALERT Eduardo
+        if(montoArray[0] == null && montoArray.length == 1){
+                alert('No tiene suficientes fondos para hacer un retiro'); // AQUI TIENES QUE APLICAR LO DE LA LIBRERIA DE SWEETALERT Eduardo
+        } else {
+            montoArray.push(-1*monto);
+            descripcionArray.push('Retiro');
+            window.localStorage.setItem('montoArray', montoArray);
+            window.localStorage.setItem('descripcionArray', descripcionArray);
+            window.localStorage.setItem('fechaArray', fechaArray);
+            console.log(montoArray);
+            console.log(descripcionArray);
+            console.log(fechaArray);
+        }
     } else {
-        montoArray.push(-1*monto);
-        window.localStorage.setItem('montoArray', montoArray);
-        console.log(montoArray);
+        alert('No tiene suficientes fondos para hacer un retiro'); // AQUI TIENES QUE APLICAR LO DE LA LIBRERIA DE SWEETALERT Eduardo
     }
 
 }
